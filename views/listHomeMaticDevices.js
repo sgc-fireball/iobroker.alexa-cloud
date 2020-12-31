2f10d72608d1046ce7632874bd2f1bbdc5cf421c
Richard Hülsberg <richard.huelsberg@ort-online.net>
2020-12-31 14:41:59 +0100

    update

--- views/listHomeMaticDevices.js
+++ views/listHomeMaticDevices.js
@@ -1,13 +1,14 @@
 let fnc = function (doc) {
     if (doc._id.match(/^hm-rpc\.[0-9]+\.[A-Za-z0-9_-]+$/)) {
         emit(doc._id, doc);
-        /*if (!!doc.value && !!doc.value.common && !!doc.value.native) {
-            if (!!doc.value.common.name && !!doc.value.native.TYPE && !!doc.value.native.ADDRESS) {
+        /*if (!!doc && !!doc.common && !!doc.native) {
+            if (!!doc.common.name && !!doc.native.TYPE && !!doc.native.ADDRESS) {
                 emit(doc._id, {
                     id: doc._id,
-                    name: doc.value.common.name,
-                    type: doc.value.native.TYPE,
-                    address: doc.value.native.ADDRESS,
+                    name: doc.common.name,
+                    type: doc.native.TYPE,
+                    address: doc.native.ADDRESS,
+                    version: doc.native.VERSION
                 });
             }
         }*/

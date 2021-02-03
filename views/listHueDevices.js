62a6a358831914777c7c0b47c224d589f806ca4f
Richard Hülsberg <richard.huelsberg@ort-online.net>
2021-02-03 23:31:37 +0100

    implement philips hue adapter devices

--- views/listHueDevices.js
+++ views/listHueDevices.js
@@ -1,5 +1,5 @@
 let fnc = function(doc) {
-    if (doc._id.match(/^(hue-extended\.[0-9]+\.lights\.[^\.]+)$/)) {
+    if (doc._id.match(/^hue-extended\.[0-9]+\.lights\.[^\.]+$/)) {
         if (!!doc && !!doc.common && !!doc.common.name) {
             emit(doc._id, {
                 id: doc._id,

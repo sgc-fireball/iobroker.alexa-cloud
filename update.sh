0ab64be954a592f9ab9290b82b8b87e2c63ee6bc
Richard Hülsberg <richard.huelsberg@twt.de>
2026-01-03 22:37:00 +0100

    feat(upgrade): NO-TICKET
    improve the register device process

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ cd $(dirname $0)
 
 for FILE in $(ls views); do
   NAME=$(echo "${FILE:0:-3}")
-  CONTENT=$(cat "views/${FILE}")
-  CONTENT=$(echo "${CONTENT:10:-1}")
+  CONTENT=$(cat "views/${FILE}" | tr "\r" " " | tr "\n" " " | sed -r 's/\s\s+/ /g')
+  CONTENT=$(echo "${CONTENT:10:-2}" | sed -r 's/function \(doc/function(doc/g')
 
   TMPFILE=$(mktemp)
   jq --arg CONTENT "${CONTENT}" ".objects[0].views.${NAME}.map = \$CONTENT" "io-package.json" > "${TMPFILE}"
